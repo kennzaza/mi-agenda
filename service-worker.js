10f1772d009f837c5907d4705d03685be5f7a52f
kennzaza <braianpro464@gmail.com>
2026-09-13 12:03:43 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "agenda-cache-v1";
-const ASSETS = ["./agenda.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+const ASSETS = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
